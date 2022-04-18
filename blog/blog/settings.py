9642039e055bfdfd2003b2e8924ea0c1c49ac6ef
341504sunhongbo <2506391058@qq.com>
2022-04-18 17:43:09 +0800

    静态文件添加和user文件夹创建

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -150,6 +150,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+#配置静态文件路径
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 #日志信息
 LOGGING = {
